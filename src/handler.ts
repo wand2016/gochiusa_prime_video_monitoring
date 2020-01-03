a803c789c012c7aa58accad31de597d78429f1c1
Daiki Horiyama <d19921207@gmail.com>
2020-01-03 16:51:10 +0900

    Serverless Framework導入

---
+++ src/handler.ts
@@ -0,0 +1,25 @@
+import container from "@app/inversify.config";
+import { TYPES } from "@app/types";
+import GochiUsaMonitoring from "@app/Services/GochiUsaMonitoring";
+
+export const entrypoint = async () => {
+    const service = container.get<GochiUsaMonitoring>(TYPES.GochiUsaMonitoring);
+
+    try {
+        service.monitorContentsAndPublishMetrics();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "ok",
+            }, null, 2),
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "boo",
+                error: e,
+            }, null, 2),
+        }
+    }
+};
